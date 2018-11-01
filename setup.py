eb383462fd3797f2ab6b8b55964cab0aa27efa53
ntu-trial <41179970+ntu-trial@users.noreply.github.com>
2018-11-01 10:12:13 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+package_name = "example-python"
+package_version = "1.0.0"
+
+setup(
+    name=package_name,
+    version=package_version,
+    author="some author",
+    install_requires=["requests", "numpy", "Django==1.9.6"],
+        ]
+    }
+)
